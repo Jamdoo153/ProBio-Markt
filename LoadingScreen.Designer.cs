9b4d6991ee98e9ca94843f380ffbe3fecbdee41f
Michael Walke <Jamdoo@gmx.de>
2023-03-11 16:42:02 +0100

    LoadingProgressBar hinzu

--- LoadingScreen.Designer.cs
+++ LoadingScreen.Designer.cs
@@ -28,21 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoadingScreen));
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.LoadingProgressBar = new System.Windows.Forms.ProgressBar();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.lblLoadingProgress = new System.Windows.Forms.Label();
+            this.LoadingBarTimer = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // progressBar1
+            // LoadingProgressBar
             // 
-            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.progressBar1.Location = new System.Drawing.Point(12, 225);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(700, 37);
-            this.progressBar1.TabIndex = 0;
+            this.LoadingProgressBar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.LoadingProgressBar.Location = new System.Drawing.Point(12, 225);
+            this.LoadingProgressBar.Name = "LoadingProgressBar";
+            this.LoadingProgressBar.Size = new System.Drawing.Size(700, 37);
+            this.LoadingProgressBar.TabIndex = 0;
             // 
             // pictureBox1
             // 
@@ -65,17 +67,22 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "Laoding";
             // 
-            // label4
+            // lblLoadingProgress
             // 
-            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
-            this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(387, 202);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(32, 20);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "0%";
+            this.lblLoadingProgress.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblLoadingProgress.AutoSize = true;
+            this.lblLoadingProgress.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.lblLoadingProgress.ForeColor = System.Drawing.Color.White;
+            this.lblLoadingProgress.Location = new System.Drawing.Point(387, 202);
+            this.lblLoadingProgress.Name = "lblLoadingProgress";
+            this.lblLoadingProgress.Size = new System.Drawing.Size(32, 20);
+            this.lblLoadingProgress.TabIndex = 3;
+            this.lblLoadingProgress.Text = "0%";
+            // 
+            // LoadingBarTimer
+            // 
+            this.LoadingBarTimer.Interval = 40;
+            this.LoadingBarTimer.Tick += new System.EventHandler(this.LoadingBarTimer_Tick);
             // 
             // LoadingScreen
             // 
@@ -84,15 +91,16 @@
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
             this.ClientSize = new System.Drawing.Size(728, 274);
             this.ControlBox = false;
-            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblLoadingProgress);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.LoadingProgressBar);
             this.Controls.Add(this.pictureBox1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "LoadingScreen";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "LoadingScreen";
+            this.Load += new System.EventHandler(this.LoadingScreen_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -101,10 +109,11 @@
 
         #endregion
 
-        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar LoadingProgressBar;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblLoadingProgress;
+        private System.Windows.Forms.Timer LoadingBarTimer;
     }
 }
 
--- LoadingScreen.cs
+++ LoadingScreen.cs
@@ -12,9 +12,29 @@ namespace Pro_Bio_Markt
 {
     public partial class LoadingScreen : Form
     {
+        private int loadingBarValue;
         public LoadingScreen()
         {
             InitializeComponent();
         }
+        private void LoadingScreen_Load(object sender, EventArgs e)
+        {
+            LoadingBarTimer.Start();
+        }
+        private void LoadingBarTimer_Tick(object sender, EventArgs e)
+        {
+            loadingBarValue += 1;
+
+            lblLoadingProgress.Text = loadingBarValue.ToString() +"%";
+            LoadingProgressBar.Value = loadingBarValue;
+
+            if (loadingBarValue >= LoadingProgressBar.Maximum)
+            { 
+                LoadingBarTimer.Stop();
+            }
+
+        }
+
+        
     }
 }
